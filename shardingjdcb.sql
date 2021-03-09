e2be50bb013180edd08d2b1ffb5b58551e31d50a
wangkuan <wangkuan@harmonycloud.cn>
2021-03-09 23:46:34 +0800

    包调整

--- shardingjdcb.sql
+++ shardingjdcb.sql
@@ -58,6 +58,19 @@ CREATE TABLE `order_item`
   AUTO_INCREMENT = 1
   DEFAULT CHARSET = utf8mb4 COMMENT ='订单表';
 
-CREATE TABLE
+CREATE TABLE `warehouse`
+(
+    `id`                bigint(20)  NOT NULL AUTO_INCREMENT,
+    `warehouse_name`    varchar(64) NOT NULL COMMENT '仓库名',
+    `warehouse_code`    varchar(64) NOT NULL COMMENT '仓库编码',
+    `warehouse_address` varchar(512)         default '' COMMENT '仓库地址',
+    `gmt_create`        datetime             DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `gmt_modified`      datetime    NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `warehouse_code_idx` (`warehouse_code`) USING BTREE,
+    KEY `warehouse_warehouse_name_IDX` (`warehouse_name`) USING BTREE
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  DEFAULT CHARSET = utf8mb4 COMMENT ='仓库信息表';
 
 
